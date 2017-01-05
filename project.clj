882da999cf2e781f6788bce2b1d5e52d6a3a4c90
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-01-05 17:43:04 +0100

    updated deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.domaindrivenarchitecture/cryogen-markdown "0.1.5-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/cryogen-markdown "0.1.6-SNAPSHOT"
   :description "Markdown parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-markdown"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.domaindrivenarchitecture/cryogen-core "0.1.47-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/cryogen-core "0.1.48-SNAPSHOT"]
                  [markdown-clj "0.9.89"]]
   :deploy-repositories [["snapshots" :clojars]
                         ["releases" :clojars]])
