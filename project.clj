eb0bda773b5c5d58db4bdfda9923644683659b4e
jem <michael.jerger@meissa-gmbh.de>
2019-04-24 18:27:13 +0200

    Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.domaindrivenarchitecture/cryogen-markdown "0.1.6-SNAPSHOT"
+(defproject dda/cryogen-markdown "0.1.6"
   :description "Markdown parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-markdown"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.domaindrivenarchitecture/cryogen-core "0.1.53.1-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [dda/cryogen-core "0.1.53"]
                  [markdown-clj "1.0.7"]]
   :deploy-repositories [["snapshots" :clojars]
                         ["releases" :clojars]])
